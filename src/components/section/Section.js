acd176efdfaadd690339aeef026e1aedc721ba69
Evgen <sport777life@gmail.com>
2023-04-10 21:04:17 +0200

    add form

--- src/components/section/Section.js
+++ src/components/section/Section.js
@@ -4,6 +4,22 @@ const Section = () => {
 
     return (
         <section className={styles.section}>
+            <h1 className={styles.title}>Notes</h1>
+            <form className={styles.form}>
+                <p>Or log in using an email and password, after registering:</p>
+                <div>
+                    <label>Email:</label>
+                    <input placeholder='your@email.com' type='email'/>
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input placeholder='password' type='password'/>
+                </div>
+                <div className={styles.groupButton}>
+                    <button type='submit'>Log in</button>
+                    <button type='submit'>Registration</button>
+                </div>
+            </form>
         </section>
     )
 
